c3fbf92fbfab0e265268151958dbe5681ed0dce7
etiennelancelle <elancelle.pro@gmail.com>
2024-03-05 10:21:59 +0100

    clean and doc

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+"""
+Main module of this little workaround about LLMs.
+"""
 from dotenv import dotenv_values
 
 CONFIG = dotenv_values('.env')
-
-print(CONFIG['CLAUDE_API_KEY'])
+if __name__ == '__main__':
+    if CONFIG['CLAUDE_API_KEY']:
+        print('Claude OK, enjoy !')
+    else:
+        print('Please provide a .env file, with a Claude API key, formatted like in .env-example')
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
-# TODO :
+# User guide :
 
-1. Create a virtual environment. 
+1. Create a virtual environment.
 2. Install requirements.
 3. Create a ".env" file, and fill it with you API keys, just like in ".env-example".
-4. Run "main.py".
+4. Use the notebook.ipynb for your tests.
